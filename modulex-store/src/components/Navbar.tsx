a2da1ba0913b2f7d06590ab0ef8f8af1095a7be8
devaeterne <info@safaksariyildiz.com>
2026-08-28 11:51:11 +0200

    Point navigation to Products catalog

--- modulex-store/src/components/Navbar.tsx
+++ modulex-store/src/components/Navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" href="/shop" onClick={handleLinkClick}>
+            <Link className="nav-link" href="/products" onClick={handleLinkClick}>
               Products
             </Link>
           </li>
