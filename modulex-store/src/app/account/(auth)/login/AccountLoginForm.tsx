3b98312bbbedbe55e90ab12676e0ec01b2b5f52d
devaeterne <info@safaksariyildiz.com>
2026-08-28 21:35:57 +0200

    fix: use unified account password recovery

--- modulex-store/src/app/account/(auth)/login/AccountLoginForm.tsx
+++ modulex-store/src/app/account/(auth)/login/AccountLoginForm.tsx
@@ -19,7 +19,7 @@ export default function AccountLoginForm() {
         <input id="account-password" className="form-control" type="password" name="password" autoComplete="current-password" disabled={pending} required />
       </div>
       <div className="d-flex justify-content-end mb-4">
-        <Link href="/dealer/forgot-password" className="small text-secondary">Forgot password?</Link>
+        <Link href="/account/forgot-password" className="small text-secondary">Forgot password?</Link>
       </div>
       <button className="btn btn-dark w-100" type="submit" disabled={pending}>{pending ? "Signing in…" : "Sign in"}</button>
     </form>
