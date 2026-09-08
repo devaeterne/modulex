d8172d21e4cf368faf8dd13f18bdd24c3c4083df
devaeterne <info@safaksariyildiz.com>
2026-09-08 15:25:08 +0200

    feat(project): wire P6 selector into accepted Proposal tab

--- modulex-admin/src/components/customers/project-detail/ProjectProposalTab.tsx
+++ modulex-admin/src/components/customers/project-detail/ProjectProposalTab.tsx
@@ -20,6 +20,7 @@ import {
 import ProjectProposalAreaList from "./ProjectProposalAreaList";
 import ProjectProposalEditor from "./ProjectProposalEditor";
 import ProjectProposalLifecycleActions from "./ProjectProposalLifecycleActions";
+import ProjectProposalOrderConversion from "./ProjectProposalOrderConversion";
 import ProjectProposalPricingGroups from "./ProjectProposalPricingGroups";
 import ProjectProposalRevisionHistory from "./ProjectProposalRevisionHistory";
 
@@ -214,6 +215,9 @@ export default function ProjectProposalTab({ projectId }: { projectId: string })
       {proposal && selectedRevision ? (
         <>
           <ProjectProposalLifecycleActions proposal={proposal} canManage={canManage} onChanged={refreshSelected} />
+          {selectedRevision.state === "accepted" ? (
+            <ProjectProposalOrderConversion projectId={projectId} revisionId={selectedRevision.id} canManage={canManage} />
+          ) : null}
           {!editable && canManage ? (
             <Alert variant="info" title="Revision locked" message="This revision cannot be edited in place. Use the Proposal Lifecycle actions for sent revisions; accepted commercial changes belong in Project Change Orders." />
           ) : null}
